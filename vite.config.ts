959488601757f31a1d9f411349e7b58cde7342bf
HeliumNeon_ <helium2neon10@gmail.com>
2026-03-04 21:21:55 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: './', // Use relative paths for portability
-    plugins: [react(), tailwindcss(), viteSingleFile()],
+    plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
